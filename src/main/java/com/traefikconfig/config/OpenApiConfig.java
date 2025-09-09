1ba99d97b01d647f305075e5d464c727dd2c9b7d
Vijay Pradhan <58530749+VijayIPradhan@users.noreply.github.com>
2025-09-09 16:29:54 +0530

    Update OpenApiConfig.java

--- src/main/java/com/traefikconfig/config/OpenApiConfig.java
+++ src/main/java/com/traefikconfig/config/OpenApiConfig.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -21,14 +25,13 @@ public class OpenApiConfig {
                                 .name("Traefik Config Manager Support")
                                 .email("support@example.com")
                                 .url("https://trcon.devcrm.seabed2crest.com"))
-                        .servers(List.of(
-                                    new Server().url("https://trcon.devcrm.seabed2crest.com").description("Production Server"),
-                                    new Server().url("http://localhost:8080").description("Local Development Server")
-                ))
                         .license(new License()
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT"))
-                );
+                )
+                .servers(Arrays.asList( // Use Arrays.asList for Java 8 compatibility
+                        new Server().url("https://trcon.devcrm.seabed2crest.com").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Development Server")
+                ));
     }
 }
-
